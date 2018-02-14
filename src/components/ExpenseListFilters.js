c1179b2a3dc15dc32fc7868822a4d195a2b242d5
pb3975 <metzwa.2@gmail.com>
2018-02-13 23:26:59 -0500

    Added a summary message to the Dashboard

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -64,7 +64,7 @@ const mapStateToProps = (state) => ({
 
 
 const mapDispatchToProps = (dispatch) => ({
-  setTextFilter: (text) => dipatch(setTextFilter(text)),
+  setTextFilter: (text) => dispatch(setTextFilter(text)),
   sortByDate: () => dispatch(sortByDate()),
   sortByAmount: () => dispatch(sortByAmount()),
   setStartDate: (startDate) => dispatch(setStartDate(startDate)),
